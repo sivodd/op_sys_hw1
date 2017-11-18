c7d490550e13606b6b82d87d3cfe2e65b50747c2
sivan <sivanoddes@gmail.com>
2017-11-18 23:19:50 +0200

    added cpp files

---
+++ cpp_smash/signals.cpp
@@ -0,0 +1,8 @@
+// signals.c
+// contains signal handler funtions
+// contains the function/s that set the signal handlers
+
+/*******************************************/
+/* Name: handler_cntlc
+   Synopsis: handle the Control-C */
+#include "signals.h"
---
+++ cpp_smash/signals.h
@@ -0,0 +1,13 @@
+#ifndef _SIGS_H
+#define _SIGS_H
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+
+
+#endif
